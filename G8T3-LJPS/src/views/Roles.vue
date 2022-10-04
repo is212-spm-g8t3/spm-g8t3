ebeeeea3fce0a208c0524e76e9b11de02ee54089
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2022-10-05 00:16:54 +0800

    Added Create Role Modal in Roles.vue

--- G8T3-LJPS/src/views/Roles.vue
+++ G8T3-LJPS/src/views/Roles.vue
@@ -41,10 +41,51 @@
 		<!-- / Create Role Modal Pop up -->
 		<template>
 			<div>
-				<a-modal centered v-model="visible" title="Create Role" @ok="handleOk">
-					<p>Some contents...</p>
-					<p>Some contents...</p>
-					<p>Some contents...</p>
+				<a-modal centered v-model="visible" title="Create Role" @cancel="handleCancel">
+					<template slot="footer">
+						<a-button key="back" @click="handleCancel">
+						Cancel
+						</a-button>
+						<a-button key="submit" type="primary" :loading="loading" @click="handleCreate">
+						Create
+						</a-button>
+					</template>
+
+					<a-form-model layout="vertical" ref="ruleForm" :model="form" :rules="rules">
+						<a-row gutter="16">
+							<a-col :span="12">
+								<a-form-model-item slot="" label="Role Name" prop="name">
+									<a-input v-model="form.name" placeholder="E.g. Software Developer" />
+								</a-form-model-item>
+							</a-col>
+							<a-col :span="12">
+								<a-form-model-item label="Department" prop="department">
+									<a-input v-model="form.department" placeholder="E.g. Technology" />
+								</a-form-model-item>
+							</a-col>
+						</a-row>
+
+						<a-row>
+							<a-form-model-item label="Role Description" prop="description">
+								<a-input v-model="form.description" type="textarea" />
+							</a-form-model-item>
+						</a-row>
+						<a-row>
+							<a-form-model-item label="Skills" prop="skills">
+								<a-select
+									mode="multiple"
+									style="width: 100%"
+									placeholder="Select"
+								>
+									<a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
+									{{ (i + 9).toString(36) + i }}
+									</a-select-option>
+								</a-select>
+							</a-form-model-item>
+						</a-row>
+
+					</a-form-model>
+
 				</a-modal>
 			</div>
 		</template>
@@ -56,7 +97,10 @@
 <script>
 
 	// "Authors" table component.
+	import Vue from "vue";
 	import CardRoleTable from '../components/Cards/CardRoleTable' ;
+	import { FormModel } from 'ant-design-vue';
+	Vue.use(FormModel);
 	
 	// "Authors" table list of columns and their properties.
 	const table1Columns = [
@@ -180,17 +224,52 @@
 
 				// Associating "Authors" table columns with its corresponding property.
 				table1Columns: table1Columns,
+				titleName: "Roles",
 				visible: false,
-				titleName: "Roles"
+				loading: false,
+				modalLayout: "vertical",
+				skillsOptions: ['Skill 1', 'Skill 2', 'Skill 3'],
+				form: {
+					name: '',
+					department: '',
+					description: '',
+					skills: []
+				},
+				rules: {
+					name: [{ required: true, message: 'Name is required!'}],
+					department: [{ required: true, message: 'Department is required!'}],
+					description: [{ required: true, message: 'Description is required!'}],
+					skills: [{type:'array', required: true, message: 'Please input at least one skill!'}]
+				}
 			}
 		},
 		methods: {
 			showModal() {
-			this.visible = true;
+				this.visible = true;
 			},
-			handleOk(e) {
-			console.log(e);
-			this.visible = false;
+
+			handleCreate(e) {
+				console.log(e);
+				this.loading = true;
+				// Perform check with database whether role is in database
+				// If not, add to database
+				// this.loading = false and this.visible = false
+				// Show green alert bar if added successfully
+
+				this.$refs.ruleForm.validate(valid => {
+					if (valid) {
+						alert('submit!');
+						this.visible = false;
+					} else {
+						console.log('error submit!!');
+						return false;
+					}
+				});
+			},
+
+			handleCancel(e) {
+				this.visible = false;
+      			this.$refs.ruleForm.resetFields();
 			},
 		},
 	})
