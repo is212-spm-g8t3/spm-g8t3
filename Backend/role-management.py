7e1852dc94d3a7b0ab1d804ef922d606f4fb2ff0
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2022-10-09 15:57:32 +0800

    Update getRolesWithSkills function

--- Backend/role-management.py
+++ Backend/role-management.py
@@ -55,35 +55,34 @@ def get_role():
 
 @app.route('/getRoleWithSkills', methods=['GET'])
 def get_role_with_skills():
-    # try:
-    #     # data = job_role.query.join(job_role_skills, job_role.Job_Role_ID == job_role_skills.Job_Role_ID).join(Skill, job_role_skills.Skill_ID == Skill.Skill_ID)
-    #     data = job_role.query(job_role.Job_Role_ID, job_role.Job_Role_Name, job_role.Job_Role_Description, Skill.Skill_ID, Skill.Skill_Name).join(job_role_skills, job_role.Job_Role_ID == job_role_skills.Job_Role_ID).join(Skill, job_role_skills.Skill_ID == Skill.Skill_ID)
-    #     print(data)
-    #     return jsonify(
-    #         {
-    #             "code": 200,
-    #             "data": {
-    #                 "allRolesWithSkills": [item.json() for item in data]
-    #             }
-    #         }
-    #     )
-    # except Exception as e:
-    #     return jsonify(
-    #         {
-    #             "code": 404,
-    #             "message": "Error in retrieving data."
-    #         }
-    #     )
+    try:
+        data = db.session.query(job_role, Skill, job_role_skills).filter(job_role.Job_Role_ID == job_role_skills.Job_Role_ID, job_role_skills.Skill_ID == Skill.Skill_ID).with_entities(job_role.Job_Role_ID, job_role.Job_Role_Name, job_role.Job_Role_Description, Skill.Skill_Name, Skill.Skill_Description)
+        print(data)
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "allRolesWithSkills": [dict(item) for item in data]
+                }
+            }
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 404,
+                "message": "Error in retrieving data."
+            }
+        )
     # data = job_role.query(job_role.Job_Role_ID, job_role.Job_Role_Name, job_role.Job_Role_Description, Skill.Skill_ID, Skill.Skill_Name).join(job_role_skills, job_role.Job_Role_ID == job_role_skills.Job_Role_ID).join(Skill, job_role_skills.Skill_ID == Skill.Skill_ID)
     # print(data)
 
-    all_roles = [dict(item.json()) for item in job_role.query.all()]
-    for each_role in all_roles:
-        each_role["skills"] = []
-        role_skill = [item for item in job_role_skills.query.filter(job_role_skills.Job_Role_ID == each_role['Role_ID']).with_entities(job_role_skills.Skill_ID).all()]
-        print(role_skill)
-    print(all_roles)
-    return "non"
+    # all_roles = [dict(item.json()) for item in job_role.query.all()]
+    # for each_role in all_roles:
+    #     each_role["skills"] = []
+    #     role_skill = [item for item in job_role_skills.query.filter(job_role_skills.Job_Role_ID == each_role['Role_ID']).with_entities(job_role_skills.Skill_ID).all()]
+    #     print(role_skill)
+    # print(all_roles)
+    # return "non"
 
 
 @app.route('/createRole', methods=['POST'])
