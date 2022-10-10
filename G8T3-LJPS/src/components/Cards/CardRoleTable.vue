67c605386249b2beace694bc326c2e9f82ff3541
Maximilian Tan <maximiliantanyx@gmail.com>
2022-10-10 09:16:34 +0800

    undid commits for roles

--- G8T3-LJPS/src/components/Cards/CardRoleTable.vue
+++ G8T3-LJPS/src/components/Cards/CardRoleTable.vue
@@ -18,11 +18,11 @@
 		</template>
 		<a-table :columns="columns" :data-source="dataFilteredStatus" :pagination="true">
 
-			<template slot="Job_Role_Name" slot-scope="Job_Role_Name">
+			<template slot="roleName" slot-scope="roleName">
 				<div class="table-avatar-info">
-					<!--<a-avatar shape="square" :src="Job_Role_Name.avatar" />-->
+					<a-avatar shape="square" :src="roleName.avatar" />
 					<div class="avatar-info" style="margin-top: 7px">
-						<h6>{{ Job_Role_Name }}</h6>
+						<h6>{{ roleName.name }}</h6>
 					</div>
 				</div>
 			</template>
@@ -41,11 +41,8 @@
 			</template>
 
 			<template slot="editBtn" slot-scope="row">
-				<a-button type="link" :data-id="row.key">
-					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path class="fill-gray-7" d="M13.5858 3.58579C14.3668 2.80474 15.6332 2.80474 16.4142 3.58579C17.1953 4.36683 17.1953 5.63316 16.4142 6.41421L15.6213 7.20711L12.7929 4.37868L13.5858 3.58579Z"/>
-						<path class="fill-gray-7" d="M11.3787 5.79289L3 14.1716V17H5.82842L14.2071 8.62132L11.3787 5.79289Z"/>
-					</svg>
+				<a-button type="default" :data-id="row.key" @click="updateRoles(data[row.key-1])">
+					Edit
 				</a-button>
 			</template>
 
@@ -56,37 +53,43 @@
 </template>
 
 <script>
-
-	export default ({
-		props: {
-			data: {
-				type: Array,
-				default: () => [],
-			},
-			columns: {
-				type: Array,
-				default: () => [],
-			},
-			titleName: {
-				type: String,
-				default: ""
-			}
+import structuredClone from '@ungap/structured-clone';
+export default ({
+	props: {
+		data: {
+			type: Array,
+			default: () => [],
 		},
-		data() {
-			return {
-				// Active button for the "Authors" table's card header radio button group.
-				statusRadioBtn: 'all',
-			}
+		columns: {
+			type: Array,
+			default: () => [],
 		},
-
-		computed: {
-			dataFilteredStatus: function() {
-				if (this.statusRadioBtn != 'all') {
-					return this.data.filter(eachData => eachData.status == this.statusRadioBtn)
-				}
-				return this.data
-			}
+		titleName: {
+			type: String,
+			default: ""
 		}
-	})
+	},
+	data() {
+		return {
+			// Active button for the "Authors" table's card header radio button group.
+			statusRadioBtn: 'all',
+		}
+	},
+
+	computed: {
+		dataFilteredStatus: function() {
+			if (this.statusRadioBtn != 'all') {
+				return this.data.filter(eachData => eachData.status == this.statusRadioBtn)
+			}
+			return this.data
+		}
+	},
+
+	methods: {
+		updateRoles(currentRowData) {
+			this.$emit('updateRecord', structuredClone(currentRowData))
+		}
+	}
+})
 
 </script>
