86f2a0b42184076076730dadb3baa8bdf46f604b
quentinquek <quentinquek@hotmail.com>
2022-10-29 02:28:07 +0800

    test

--- Backend/unit_test.py
+++ Backend/unit_test.py
@@ -5,24 +5,22 @@ from ljps import *
 
 class TestApp(flask_testing.TestCase):
 
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
-    TESTING = True
+    # Setting a in-memory temporary database
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
+    app.config['TESTING'] = True
     
     def create_app(self):
-        # Setting a in-memory temporary database
-        app = Flask(__name__)
-        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
-        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
-        app.config['TESTING'] = True
-        db = SQLAlchemy(app)
         return app
 
     # Run below each test function (method)
     def setUp(self):
+        print("hello")
         db.create_all()
 
     # Destroy after each test function (method)
     def tearDown(self):
+        print("byebye")
         db.session.remove()
         db.drop_all()
     
