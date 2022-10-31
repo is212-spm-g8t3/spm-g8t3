6f0de5b29a5fc16dd40b3a0756ecdd59d1178773
quentinquek <quentinquek@hotmail.com>
2022-10-31 22:44:19 +0800

    testing

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from . import db_creds
+from flask_cors import CORS
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 # def create_app():
 
 app = Flask(__name__)
+CORS(app)
 # Database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://' + db_creds.username + ':' + db_creds.password + '@' + db_creds.hostname + ':3306/ljps'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
-db = SQLAlchemy(app)
+db.init_app(app)
 
 from Backend import routes
 from Backend import models
