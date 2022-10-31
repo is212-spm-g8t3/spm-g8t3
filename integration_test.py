27a6ff561f864af6b327adef23f8599ed6b5fdbc
quentinquek <quentinquek@hotmail.com>
2022-10-31 23:22:02 +0800

    test

--- integration_test.py
+++ integration_test.py
@@ -1,16 +1,17 @@
 import unittest
-import flask_testing
+# import flask_testing
 from Backend import app, db
 # from app import app
 from Backend.models import Courses_Catalog, Skill, course_skills, learning_journey, learning_journey_course, learning_journey_skill, registration, job_role, job_role_skills, system_role, staff
 from datetime import datetime
 
-class TestApp(flask_testing.TestCase):
+class TestApp(unittest.TestCase):
 
     # Setting a in-memory temporary database
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://' + db_creds.username + ':' + db_creds.password + '@' + db_creds.hostname + ':3306/ljps_test'
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
+    # app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['TESTING'] = True
 
     def create_app(self):
@@ -30,8 +31,6 @@ class TestApp(flask_testing.TestCase):
         db.drop_all()
 
 ''' Test Cases for Courses '''
-
-
 class TestCourse(TestApp):
     def test_get_all_courses(self):
 
