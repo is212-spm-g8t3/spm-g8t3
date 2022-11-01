b7357dd0db614430cfc2d4a69d7e0826d06251ee
maxtyx <maximiliantanyx@gmail.com>
2022-11-01 14:26:11 +0800

    remove test

--- integration_test.py
+++ integration_test.py
@@ -32,7 +32,6 @@ class TestCourse(TestApp):
 
         # Uncomment below to see the full response of query
         # self.maxDiff = None
-        #test
 
         course1 = Courses_Catalog(
             Course_ID="COR001",
