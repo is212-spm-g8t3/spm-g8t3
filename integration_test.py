f277d255723f7bbabcfc5615213d99919408d293
maxtyx <maximiliantanyx@gmail.com>
2022-11-02 19:05:40 +0800

    Update integration_test.py

--- integration_test.py
+++ integration_test.py
@@ -1,5 +1,4 @@
 import unittest
-from urllib import response
 import flask_testing
 from flask import json
 from Backend import app, db
@@ -493,6 +492,7 @@ class TestCourse(TestApp):
 
 ''' Test Cases for Roles '''
 class TestRoles(TestApp):
+    """ Done by: Max """
     def test_get_all_roles(self):
         role1 = job_role(
             Job_Role_ID=2,
@@ -542,7 +542,8 @@ class TestRoles(TestApp):
             }
         })
 
-    def test_get_roles_with_skills(self):
+    """ Done by: Max """
+    def test_get_roles_with_skills(self): 
         role1 = job_role(
             Job_Role_ID=2,
             Job_Role_Name="Engineer",
@@ -625,9 +626,82 @@ class TestRoles(TestApp):
     def test_create_role(self):
         pass
 
+    """ Done by: Ho Zhi Ying """
+    def test_update_role(self):
+     
+        role1 = job_role(
+            Job_Role_ID=2,
+            Job_Role_Name="Engineer",
+            Job_Role_Description="Engineers, as practitioners of engineering, are professionals who invent, design, analyze, build and test machines, complex systems, structures, gadgets and materials to fulfill functional objectives and requirements while considering the limitations impo",
+            Department="Engineering",
+            Status="Active",
+            Created_Date= datetime(2022, 10, 27, 0, 0, 0))
+
+        role2 = job_role(
+            Job_Role_ID=17,
+            Job_Role_Name="Software Developer",
+            Job_Role_Description="Develop software applications",
+            Department="Technology",
+            Status="Active",
+            Created_Date= datetime(2022, 10, 18, 0, 0, 0))
+        
+        job_role_skill1 = job_role_skills(
+            Job_Role_ID=17,
+            Skill_ID=1
+        )
+
+        job_role_skill2 = job_role_skills(
+            Job_Role_ID=17,
+            Skill_ID=2
+        )
+
+        db.session.add(role1)
+        db.session.add(role2)
+        db.session.add(job_role_skill1)
+        db.session.add(job_role_skill2)
+        db.session.commit()
+
+        # Change the name (Happy path)
+        updateForm = {
+        	'id': 17,
+        	'name': 'Full Stack Developer',
+        	'description': 'Work on both front-end and back-end application.',
+        	'department': 'Technology',
+        	'status': 'Active',
+        	'skills': [3, 4]
+        }
+
+        response = self.client.post("/updateRole",
+            content_type='application/json',
+            data=json.dumps(updateForm))
+
+        self.assertEqual(response.json, {
+            "code": 201,
+            "message": 'Successfully updated role.'
+        })
+
+        # Change to exact name of another role (Negative Case)
+        updateForm = {
+        	'id': 17,
+        	'name': 'Engineer',
+        	'description': 'Develop software applications',
+        	'department': 'Technology',
+        	'status': 'Active',
+        	'skills': [3, 4]
+        }
+
+        response = self.client.post("/updateRole",
+            content_type='application/json',
+            data=json.dumps(updateForm))
+
+        self.assertEqual(response.json, {
+            "code": 400,
+            "message": "Role already exists. Please use another role name."
+        })
 
 ''' Test Cases for Skills '''
 class TestSkills(TestApp):
+    """ Done by: Max """
     def test_get_all_skills(self):
         skill1 = Skill(
             Skill_ID=2,
@@ -677,8 +751,69 @@ class TestSkills(TestApp):
             }
         })
 
+    """ Done by: Quentin """
     def test_get_skills_by_role(self):
-        pass
+        
+        skill1 = Skill(
+            Skill_ID='1',
+            Skill_Name='Sales',
+            Skill_Description='Sell',
+            Skill_Type='Active',
+            Status='Active',
+            Created_Date= datetime(2022, 10, 27, 0, 0, 0)
+        )
+
+        skill2 = Skill(
+            Skill_ID='2',
+            Skill_Name='HR',
+            Skill_Description='Human Resource',
+            Skill_Type='Active',
+            Status='Active',
+            Created_Date= datetime(2022, 10, 27, 0, 0, 0)
+        )
+
+        job_role_skill1 = job_role_skills(
+            Job_Role_ID=17,
+            Skill_ID=1
+        )
+
+        job_role_skill2 = job_role_skills(
+            Job_Role_ID=17,
+            Skill_ID=2
+        )
+
+        db.session.add(skill1)
+        db.session.add(skill2)
+        db.session.add(job_role_skill1)
+        db.session.add(job_role_skill2)
+        db.session.commit()
+
+        response = self.client.get("/skills-by-role?roleId=17",
+            content_type='application/json')
+
+        self.assertEqual(response.json, {
+            "code": 200,
+            "data": {
+                "skills": [
+                    {
+                        'Skill_ID': 1,
+                        'Skill_Name': 'Sales',
+                        'Skill_Description': 'Sell',
+                        'Skill_Type': 'Active',
+                        'Status': 'Active',
+                        'Created_Date': 'Thu, 27 Oct 2022 00:00:00 GMT'
+                    },
+                    {
+                        'Skill_ID': 2,
+                        'Skill_Name': 'HR',
+                        'Skill_Description': 'Human Resource',
+                        'Skill_Type': 'Active',
+                        'Status': 'Active',
+                        'Created_Date': 'Thu, 27 Oct 2022 00:00:00 GMT'
+                    }
+                ]
+            }
+        })
 
     # '/skills/AddNewSkill
     def test_create_skill(self):
@@ -709,6 +844,9 @@ class TestLearningJourney(TestApp):
         pass
 
     def test_create_learning_journey_course(self):
+        pass
+
+    def test_delete_existing_learning_journey_course(self):
         pass
 
 
