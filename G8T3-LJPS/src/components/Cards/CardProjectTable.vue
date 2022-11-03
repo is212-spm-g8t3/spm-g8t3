8faa1e44233ff18a591584605a4c92cb46c0d04b
quentinquek <quentinquek.2020@scis.smu.edu.sg>
2022-11-04 00:00:05 +0800

    Fix: Routing issues (#43)

--- G8T3-LJPS/src/components/Cards/CardProjectTable.vue
+++ G8T3-LJPS/src/components/Cards/CardProjectTable.vue
@@ -54,9 +54,24 @@
 			<!-- / Project Column -->
 		</a-row>
 		<div>
-			<a-button type="dark" onclick="history.back()" style="margin-top: 20px;">
-				Back
-			</a-button>
+			<a-row type="flex" style="margin-bottom: 10px;">
+				<a-col :span="24" :md="12" style="text-align: left;">
+					<a-button type="dark" @click="backToSkill()" style="margin-top: 20px;">
+						Back
+					</a-button>
+				</a-col>
+				<a-col :span="24" :md="12" style="text-align: right;">
+					<a-button v-if="this.$route.query.type == 'addToLJ'" @click="checkout()" type="dark" style="margin-top: 20px; margin-left: 20px;">
+						Save
+					</a-button>
+					<a-button v-else type="dark" onclick="history.back()" style="margin-top: 20px;">
+						Confirm
+					</a-button>
+				</a-col>
+			</a-row>
+			
+			
+
 		</div>
 
 
@@ -68,6 +83,8 @@
 
 
 <script>
+
+	import axios from 'axios';
 
 	export default ({
 		props: {
@@ -142,6 +159,90 @@
 				localStorage.setItem('selectedSkillsAndCourses', JSON.stringify(selectedSkillsAndCourses));
 				location.reload()
 			},
+
+			checkout(){
+				//checkout skills and courses
+				let sendData = confirm("You are about to save a learning journey. Proceed?");
+
+				if (sendData == false){
+					return
+				}
+
+				let roleId = this.$route.query.roleId
+				let skillId = this.$route.query.skillId
+
+				let selectedSkillsAndCourses = JSON.parse(localStorage.getItem('selectedSkillsAndCourses'));
+				let staffInfo = JSON.parse(localStorage.getItem('staffInfo'));
+				let staffId = staffInfo['staffId'];
+
+				let payload = { job_role_id: roleId, staff_id: staffId };
+				const path = 'http://localhost:5000/learningJourney/createLearningJourney';
+				axios.post(path, payload)
+					.then((res) => {
+						console.log(res)
+						//console.log("Learning Journey Successfully created")
+						
+						
+					})
+					.catch((error) => {
+					// eslint-disable-next-line
+					console.error(error);
+					});
+
+				//send data to save learning endpoint
+
+				// this.$router.push({
+				// 		path: '/save-learning-journey', 
+				// 	});
+
+				//save learning journey skills
+				for (let skill in selectedSkillsAndCourses){
+					console.log(skill)
+					let skillPayload = { skill_id: skill, staff_id: staffId, lj_id: 13 };
+					let skillPath = 'http://localhost:5000/learningJourney/createLearningJourneySkill';
+					axios.post(skillPath, skillPayload)
+						.then((res) => {
+							console.log("Learning Journey skill Successfully saved")
+							
+						})
+						.catch((error) => {
+						// eslint-disable-next-line
+						console.error(error);
+						});
+				}
+
+				//save learning journey courses
+				for (let skillId in selectedSkillsAndCourses){
+					for (let course of selectedSkillsAndCourses[skillId]){
+						let coursePayload = { skill_id: skillId, 
+							staff_id: staffId, 
+							reg_id: 1, 
+							course_id: course, 
+							lj_id: 13
+						};
+						let coursePath = 'http://localhost:5000/learningJourney/createLearningJourneyCourse';
+						axios.post(coursePath, coursePayload)
+							.then((res) => {
+								console.log("Learning Journey course Successfully saved")
+								
+							})
+							.catch((error) => {
+							// eslint-disable-next-line
+							console.error(error);
+							});
+					}
+					
+				}
+
+				alert("Learning Journey successfully created")
+				localStorage.removeItem('selectedSkillsAndCourses');
+				history.back()
+			},
+
+			backToSkill() {
+				localStorage.removeItem('selectedSkillsAndCourses');
+				history.back()
+			}
 		},
 		computed: {
 			dataFilteredStatus: function() {
